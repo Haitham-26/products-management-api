5971fa7b031e0b10fc0124dd36fdbdfd60aefe48
Haitham-26 <haitham.waki.work@gmail.com>
2026-07-06 19:56:22 +0300

    store products image urls during order creation

--- src/types/order/types/OrderItem.ts
+++ src/types/order/types/OrderItem.ts
@@ -4,6 +4,8 @@ import { ProductDiscount } from "../../product/types/ProductDiscount";
 export interface OrderItem {
   productId: Types.ObjectId;
   productName: string;
+  productMainImage?: string;
+  productGalleryImages?: string[];
   quantity: number;
   priceAtPurchase: number;
   discountAtPurchase?: ProductDiscount;
