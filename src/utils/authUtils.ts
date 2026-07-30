c75547d208e6800ea2f5530f193096dc2f86b3e3
Haitham-26 <haitham.waki.work@gmail.com>
2026-07-30 20:43:18 +0300

    potential fix for cookie set

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -56,7 +56,7 @@ export const createAuthSession = async (
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: MONTH_MS,
       path: "/",
     });
